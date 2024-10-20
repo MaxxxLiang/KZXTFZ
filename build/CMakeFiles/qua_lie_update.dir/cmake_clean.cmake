b6bfae8acba6977070f009161d1fc2fddb5687c5
MaxxxLiang <Max_xLiang@outlook.com>
2024-10-20 13:30:30 +0800

    第三次课堂作业，使用李群库完成旋转更新

---
+++ build/CMakeFiles/qua_lie_update.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qua_lie_update.dir/validation.cpp.o"
+  "qua_lie_update"
+  "qua_lie_update.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qua_lie_update.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- validation.cpp
+++ validation.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include<Eigen/Core>
 #include<Eigen/Geometry>
+#include "sophus/so3.h"
+#include "sophus/se3.h"
 using namespace std;
 
 int main(int argc, char **argv) {
@@ -12,12 +14,29 @@ int main(int argc, char **argv) {
     cout<<R<<endl;
     cout<<"初始四元数："<<endl;
     cout<<q.coeffs().transpose() <<endl;
+
+    //将w进行分解
+    double theta = w.norm();//模长
+    Eigen::Vector3d axis = w/theta;//转轴
+
+    //通过李群库求指数映射
+    Eigen::Matrix3d w_hat = Sophus::SO3::hat(w);//求反对称矩阵
+    Eigen::Matrix3d w_exp = Sophus::SO3::exp(w).matrix();//计算指数映射 实际上exp返回的是一个四元数 需要通过matrix()把它转换成旋转矩阵
+
+    //计算四元数乘数
+    Eigen::Quaterniond q_mult(1, w(0) / 2, w(1) / 2, w(2) / 2);
+    cout<<"四元数乘数 \n"<<q_mult.coeffs().transpose()<<endl;
     
     //旋转矩阵更新
-    
+    Eigen::Matrix3d R_new = R * w_exp;
+    cout<<"旋转矩阵更新 \n"<<R_new<<endl;
     
     //四元数更新    
-    
+    Eigen::Quaterniond q_new;
+    q_new = q * q_mult;
+    cout<<"四元数更新 \n"<<q_new.coeffs().transpose()<<endl;
+    cout<<"更新四元数对应旋转矩阵： \n"<<q_new.toRotationMatrix()<<endl;
+
 
     return 0;
 }

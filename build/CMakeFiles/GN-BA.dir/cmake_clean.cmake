808cefe4501b45bfd458185ae325b30e3ec000af
MaxxxLiang <Max_xLiang@outlook.com>
2024-10-29 18:58:55 +0800

    10-29课堂作业

---
+++ build/CMakeFiles/GN-BA.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GN-BA.dir/GN-BA.cpp.o"
+  "CMakeFiles/GN-BA.dir/GN-BA.cpp.o.d"
+  "GN-BA"
+  "GN-BA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GN-BA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

08eabf6de486ecb4e51875c0d29eca0a04452fcd
MaxLiang <Max_xLiang@outlook.com>
2024-11-30 12:52:59 +0800

    1119 homework update

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/maxliang/Project/CQU/KZXTFZ/Code/hw7-1119/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/maxliang/Project/CQU/KZXTFZ/Code/hw7-1119/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/maxliang/Project/CQU/KZXTFZ/Code/hw7-1119/catkin_ws/devel;/opt/ros/noetic' != '' else []
